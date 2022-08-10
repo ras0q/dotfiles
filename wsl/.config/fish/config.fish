538aede3a5797ad9cb0976a6edd4395d38111369
Ras96 <asymptote.k.k@gmail.com>
2022-08-11 08:49:02 +0900

    :sparkles: change theme

--- wsl/.config/fish/config.fish
+++ wsl/.config/fish/config.fish
@@ -58,4 +58,4 @@ function gf_open -d 'Repository search and open in editor'
 end
 
 # oh-my-posh
-oh-my-posh init fish --config ~/.night-owl.my.omp.json | source
+oh-my-posh init fish --config ~/.omp-theme.json | source
--- wsl_sync.sh
+++ wsl_sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat ./wsl_symboliclink.csv | awk -F "," -v P=$PWD '{ printf("sudo ln -sfT %s/wsl/%s %s\n",P,$1,$2) }' | bash
+cat ./wsl_symboliclink.csv | awk -F "," -v P=$PWD '{ printf("if [ -L %s ]; then sudo unlink %s; fi && sudo ln -sfT %s/wsl/%s %s\n",$2,$2,P,$1,$2) }' | bash
