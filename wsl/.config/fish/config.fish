3b368956a1757ace3a040b008e78bda9f22666c3
Ras96 <asymptote.k.k@gmail.com>
2022-11-06 19:47:46 +0900

    :bug: use setsid

--- wsl/.config/fish/config.fish
+++ wsl/.config/fish/config.fish
@@ -22,13 +22,13 @@ end
 
 # ssh-agent
 if ! ss -a | grep -q $SSH_AUTH_SOCK >/dev/null 2>&1
-   if test -S $SSH_AUTH_SOCK
+    if test -e $SSH_AUTH_SOCK
        echo "removing previous socket..."
        rm -f $SSH_AUTH_SOCK
-   end
-   echo "Starting SSH-Agent relay..."
-   set -x NPIPERELAY /mnt/c/Users/kira/scoop/shims/npiperelay.exe
-   eval (socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$NPIPERELAY -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+    end
+    echo "Starting SSH-Agent relay..."
+    set -x NPIPERELAY /mnt/c/Users/kira/scoop/shims/npiperelay.exe
+    eval (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$NPIPERELAY -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
 end
 
 # asdf
