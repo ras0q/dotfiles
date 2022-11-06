7a54baedac566be62cae5da2a7920ac444d20bc1
Ras96 <asymptote.k.k@gmail.com>
2022-11-06 15:43:18 +0900

    :wrench: use 1password as a ssh-agent

--- wsl/.config/fish/config.fish
+++ wsl/.config/fish/config.fish
@@ -23,6 +23,17 @@ if ! service docker status >/dev/null 2>&1
     end
 end
 
+# ssh-agent
+if ! ss -a | grep -q $SSH_AUTH_SOCK >/dev/null 2>&1
+   if test -S $SSH_AUTH_SOCK
+       echo "removing previous socket..."
+       rm -f $SSH_AUTH_SOCK
+   end
+   echo "Starting SSH-Agent relay..."
+   set -x NPIPERELAY /mnt/c/Users/kira/scoop/shims/npiperelay.exe
+   eval (socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$NPIPERELAY -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+end
+
 # functions
 # ---
 # fish key bindings
