ebcdf35900e50236c51008a79f425da530b0534a
ras0q <asymptote.k.k@gmail.com>
2023-01-20 15:31:46 +0900

    :fire: remove unused lines

--- mac/.config/fish/config.fish
+++ mac/.config/fish/config.fish
@@ -1,40 +1,6 @@
 # asdf
 source (brew --prefix asdf)/libexec/asdf.fish
 
-# functions
-# ---
-# fish key bindings
-function fish_user_key_bindings
-    bind \cg gf_open
-end
-
-# gh create & ghq get
-function ghcr
-    set -x reponame $argv[1]
-    gh repo create $reponame --private
-    ghq get git@github.com:ras0q/$reponame.git
-    cd (ghq root)/github.com/ras0q/$reponame
-    git branch -M main
-end
-
-# ghq + fzf
-function gf -d 'Repository search'
-    ghq list | fzf | read select
-    if test -n "$select"
-        cd (ghq root)/$select
-        echo "$select"
-        commandline -f repaint
-    else
-        echo "No repository selected."
-        return 1
-    end
-end
-
-# ghq + fzf + editor
-function gf_open -d 'Repository search and open in editor'
-    gf && echo "Opening" (basename (pwd)) "in $EDITOR" && $EDITOR .
-end
-
 # oh-my-posh
 oh-my-posh init fish --config ~/.omp-theme.json | source
 
@@ -44,5 +10,5 @@ zoxide init --cmd cd --hook pwd fish | source
 # renv
 status --is-interactive; and rbenv init - fish | source
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f ~/google-cloud-sdk/path.fish.inc ]; . ~/google-cloud-sdk/path.fish.inc; end
+# updates PATH for the Google Cloud SDK.
+~/google-cloud-sdk/path.fish.inc
