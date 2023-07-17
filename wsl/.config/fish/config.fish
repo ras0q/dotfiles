10f63c834da6c742d6abb0ab2be74a87915c8f44
ras0q <ras0q@users.noreply.github.com>
2023-07-17 10:56:51 +0900

    :sparkles: direnv

--- wsl/.config/fish/config.fish
+++ wsl/.config/fish/config.fish
@@ -1,6 +1,4 @@
 # setup background services for WSL2
-wsl2_setup_cron
-wsl2_setup_docker
 wsl2_setup_sshagent
 
 # fix problems for WSL2
@@ -8,9 +6,6 @@ wsl2_fix_interop
 
 # abbreviations
 fish_init_abbr
-
-# asdf
-source (brew --prefix asdf)/libexec/asdf.fish
 
 # oh-my-posh
 oh-my-posh init fish --strict --config ~/.omp-theme.json | source
