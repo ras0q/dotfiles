d1ee938daa8cf21f3e54d7ac2693985f533d4345
ras0q <ras0q@users.noreply.github.com>
2023-08-04 20:23:57 +0900

    skip installing font if exist

--- windows/tasks/0_install_fonts.ps1
+++ windows/tasks/0_install_fonts.ps1
@@ -1,4 +1,11 @@
+$output = "~/AppData/Local/Microsoft/Windows/Fonts/SourceCodePro-Regular.otf"
+
+if (Test-Path $output) {
+  Write-Host "File already exists: $output"
+  return
+}
+
 Invoke-WebRequest `
   -Uri https://github.com/adobe-fonts/source-code-pro/raw/release/OTF/SourceCodePro-Regular.otf `
-  -OutFile ~/AppData/Local/Microsoft/Windows/Fonts/SourceCodePro-Regular.otf `
+  -OutFile $output `
   -UseBasicParsing
