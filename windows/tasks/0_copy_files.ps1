2c0cc69b9512f57c32de97e3625469b7d19c7fb1
ras0q <ras0q@users.noreply.github.com>
2023-08-05 10:48:37 +0900

    backup

--- windows/tasks/0_copy_files.ps1
+++ windows/tasks/0_copy_files.ps1
@@ -1,1 +1,19 @@
-Copy-Item .\common\files\* ~ -Recurse -Force
+$now = Get-Date -UFormat "%Y%m%d-%H%M%S"
+$backupDir = ".\backup\" + $now + "\"
+if (!(Test-Path $backupDir)) {
+  New-Item -ItemType Directory -Path $backupDir
+}
+
+function Copy-Backup($source, $destination) {
+  Copy-Item $destination $backupDir -Recurse
+  Copy-Item $source $destination -Recurse
+}
+
+# Copy common files
+$commonDir = ".\common\files"
+Copy-Backup $commonDir\.wezterm.lua $HOME\.wezterm.lua
+
+# Copy OS specific files
+$windowsDir = ".\windows\files"
+$windowsGenDir = $windowsDir + "\generated"
+Copy-Backup $windowsGenDir\.gitconfig $HOME\.gitconfig
