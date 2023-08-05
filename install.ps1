f4684bdd488b55424b3127e0068e32816f0f5d33
ras0q <ras0q@users.noreply.github.com>
2023-08-05 12:03:56 +0900

    add todo

--- install.ps1
+++ install.ps1
@@ -18,3 +18,20 @@ catch {
   Write-Error $_.Exception.Message
   exit 1
 }
+
+Write-Info "Next steps:"
+# common
+Write-Info "  - [ ] Login to Microsoft Store"
+# winget
+Write-Info "  - [ ] Login to Microsoft Edge"
+Write-Info "  - [ ] Login to Visual Studio Code (GitHub)"
+Write-Info "  - [ ] Login to 1Password"
+Write-Info "  - [ ] Login to Discord"
+Write-Info "  - [ ] Login to Figma"
+Write-Info "  - [ ] Login to Google Drive"
+Write-Info "  - [ ] Clone your Obsidian vault (~/Documents/<vault-name> is recommended)"
+Write-Info "  - [ ] Login to LINE"
+Write-Info "  - [ ] Login to Slack"
+Write-Info "  - [ ] Login to Spotify"
+Write-Info "  - [ ] Import your ueli settings (from ./windows/files/generated/ueli.config.json)"
+Write-Info "  - [ ] Login to GitHub CLI"
