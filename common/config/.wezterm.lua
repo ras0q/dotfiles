da2be596cacdc893acff4e59af6bccd374220b90
ras0q <ras0q@users.noreply.github.com>
2023-09-22 01:05:13 +0900

    :wrench: wezterm window settings

--- common/config/.wezterm.lua
+++ common/config/.wezterm.lua
@@ -3,14 +3,22 @@ local wezterm = require 'wezterm'
 local config = {
   color_scheme = 'Catppuccin Latte',
   font = wezterm.font_with_fallback {
-    'SauceCodePro Nerd Font Mono',
     '源ノ角ゴシック Code JP',
   },
+  font_size = 12.0,
   default_prog = { 'pwsh' },
-  keys = {},
   use_ime = true,
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
+  window_background_opacity = 0.9,
+  keys = {
+    -- Alt+Shift+Fでフルスクリーン切り換え
+    {
+      key = 'f',
+      mods = 'SHIFT|META',
+      action = wezterm.action.ToggleFullScreen,
+    },
+  },
 }
 
 return config
