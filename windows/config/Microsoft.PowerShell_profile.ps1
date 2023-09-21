e6017687c43ed7a6dd1e32de00032441d2f5dfce
ras0q <ras0q@users.noreply.github.com>
2023-09-21 14:30:08 +0900

    add symlinks

--- windows/config/Microsoft.PowerShell_profile.ps1
+++ windows/config/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
+# Path: $PROFILE/Microsoft.PowerShell_profile.ps1
+
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 Set-PSReadLineOption -PredictionSource History
---
+++ windows/tasks/6_symbolic_links.ps1
@@ -0,0 +1,15 @@
+# Create symbolic links (Path -> Value)
+
+gsudo {
+  # Common files
+  $confDir = ".\common\config"
+  ## $HOME
+  New-Item -ItemType SymbolicLink -Path $HOME\.wezterm.lua -Value $confDir\.wezterm.lua
+
+  # OS specific files
+  $confDir = ".\windows\config"
+  ## $HOME
+  New-Item -ItemType SymbolicLink -Path $HOME\.gitconfig -Value $confDir\.gitconfig
+  ## $PROFILE
+  New-Item -ItemType SymbolicLink -Path $PROFILE -Value $confDir\Microsoft.PowerShell_profile.ps1
+}
