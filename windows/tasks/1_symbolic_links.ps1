d420ad8e56c95b4c5c4a0e8195c9e1965a0a96d7
ras0q <ras0q@users.noreply.github.com>
2023-09-21 16:02:47 +0900

    sync vscode settings

--- windows/tasks/1_symbolic_links.ps1
+++ windows/tasks/1_symbolic_links.ps1
@@ -13,6 +13,7 @@ gsudo {
   $confDir = "$baseDir\windows\config"
   ## $HOME
   New-Item -ItemType SymbolicLink -Force -Path $HOME\.gitconfig -Target $confDir\.gitconfig
+  New-Item -ItemType SymbolicLink -Force -Path $HOME\AppData\Roaming\Code\User\settings.json -Target $confDir\vscode-settings.json
   ## $PROFILE
   New-Item -ItemType SymbolicLink -Force -Path $PROFILE -Target $confDir\Microsoft.PowerShell_profile.ps1
 }
