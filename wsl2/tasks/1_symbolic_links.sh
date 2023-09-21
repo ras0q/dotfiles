617771cf66626e40deaa54438050f6938bf03943
ras0q <ras0q@users.noreply.github.com>
2023-09-21 23:46:54 +0900

    :wrench: add wsl2 sync files

--- wsl2/tasks/1_symbolic_links.sh
+++ wsl2/tasks/1_symbolic_links.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
+# Common files
 confDir="$PWD/common/config"
-sudo ln -sf $confDir/nvim $HOME/.config/nvim
-sudo ln -sf $confDir/.gittemplate.txt $HOME/.gittemplate.txt
-sudo ln -sf $confDir/starship.toml $HOME/.config/starship.toml
+## $HOME
+sudo ln -sf $confDir/.asdfrc $HOME
+sudo ln -sf $confDir/.gittemplate.txt $HOME
+## $HOME/.config
+sudo ln -sf $confDir/nvim $HOME/.config
+sudo ln -sf $confDir/starship.toml $HOME/.config
+
+# OS specific files
+confDir="$PWD/wsl2/config"
+## $HOME
+sudo ln -sf $confDir/.gitconfig $HOME
+sudo ln -sf $confDir/.wslconfig $HOME
+## /etc
+sudo ln -sf $confDir/wsl.conf /etc
