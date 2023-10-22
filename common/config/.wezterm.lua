f9be069ffa474140deb324071353cd3bfef08c6d
ras0q <ras0q@users.noreply.github.com>
2023-10-22 15:56:36 +0900

    :wrench: change default domain

--- common/config/.wezterm.lua
+++ common/config/.wezterm.lua
@@ -3,10 +3,11 @@ local wezterm = require 'wezterm'
 local config = {
   adjust_window_size_when_changing_font_size = false,
   color_scheme = 'Catppuccin Latte',
+  default_domain = 'WSL:Ubuntu',
   default_prog = { 'pwsh' },
   font = wezterm.font_with_fallback {
-    'SauceCodePro Nerd Font Mono',
     '源ノ角ゴシック Code JP',
+    'Source Code Pro'
   },
   font_size = 12.0,
   hide_tab_bar_if_only_one_tab = true,
