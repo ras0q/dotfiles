667680002fe54f7cdc586a6a9653fed26bf2b075
ras0q <ras0q@users.noreply.github.com>
2023-10-26 15:47:43 +0900

    :sparkles: add alias commands

--- windows/config/Microsoft.PowerShell_profile.ps1
+++ windows/config/Microsoft.PowerShell_profile.ps1
@@ -7,16 +7,12 @@ function Set-New-Path() {
 }
 
 function Call($batfile) {
-   cmd.exe /c "call `"${batfile}`" && set > %temp%\vars.txt"
-   Get-Content "${env:temp}\vars.txt" | Foreach-Object {
-      if ($_ -match "^(.*?)=(.*)$") {
-        Set-Content "env:\$($matches[1])" $matches[2]
-      }
-   }
-}
-
-function gf() {
-  ghq list -p | fzf | % { cd $_ }
+  cmd.exe /c "call `"${batfile}`" && set > %temp%\vars.txt"
+  Get-Content "${env:temp}\vars.txt" | Foreach-Object {
+    if ($_ -match "^(.*?)=(.*)$") {
+      Set-Content "env:\$($matches[1])" $matches[2]
+    }
+  }
 }
 
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
@@ -27,8 +23,17 @@ $env:Path += ";$HOME\AppData\Local\1Password\app\8"
 $env:Path += ";$HOME\scoop\apps\gcc\current\bin"
 # $env:Path += ";$HOME\.rye\shims"
 
-# Aliases
+# Aliases & Functions
 Set-Alias powershell pwsh
+# Git
+function ga() { git add -A && git commit }
+function gc() { git commit }
+function gs($branch) { git switch $branch }
+function gp() { git push }
+function gpp() { git pull && git bprune }
+# ghq
+function gf() { ghq list -p | fzf | ForEach-Object { Set-Location $_ } }
+function gg($repo) { ghq get $repo }
 
 # Starship
 Invoke-Expression (&starship init powershell)
