28cc4e8034a0df0f430c1a84586dbc1acd6e97d8
ras0q <ras0q@users.noreply.github.com>
2023-10-26 15:37:12 +0900

    update config

--- windows/config/Microsoft.PowerShell_profile.ps1
+++ windows/config/Microsoft.PowerShell_profile.ps1
@@ -6,12 +6,29 @@ function Set-New-Path() {
     [System.Environment]::GetEnvironmentVariable("Path", "User")
 }
 
+function Call($batfile) {
+   cmd.exe /c "call `"${batfile}`" && set > %temp%\vars.txt"
+   Get-Content "${env:temp}\vars.txt" | Foreach-Object {
+      if ($_ -match "^(.*?)=(.*)$") {
+        Set-Content "env:\$($matches[1])" $matches[2]
+      }
+   }
+}
+
+function gf() {
+  ghq list -p | fzf | % { cd $_ }
+}
+
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 Set-PSReadLineOption -PredictionSource History
 
 # Additional paths
 $env:Path += ";$HOME\AppData\Local\1Password\app\8"
 $env:Path += ";$HOME\scoop\apps\gcc\current\bin"
+# $env:Path += ";$HOME\.rye\shims"
+
+# Aliases
+Set-Alias powershell pwsh
 
 # Starship
 Invoke-Expression (&starship init powershell)
