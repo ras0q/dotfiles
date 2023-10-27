0a0d8524f4c3001c670c7e1198a8d246520e452b
ras0q <ras0q@users.noreply.github.com>
2023-10-27 18:49:28 +0900

    :arrow_up: update config

--- wsl/.config/fish/config.fish
+++ wsl/.config/fish/config.fish
@@ -7,11 +7,14 @@ wsl2_fix_interop
 # abbreviations
 fish_init_abbr
 
-# oh-my-posh
-oh-my-posh init fish --strict --config ~/.omp-theme.json | source
+# starship
+starship init fish | source
 
 # zoxide
 zoxide init --cmd cd --hook pwd fish | source
 
 # 1password
 source /home/ras/.config/op/plugins.sh
+
+# rye
+bass source ~/.rye/env
