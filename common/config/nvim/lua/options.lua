1b32bf95d99eae3952f176d76bef4504da23f014
ras0q <ras0q@users.noreply.github.com>
2023-11-15 00:50:44 +0900

    :wrench: add option

--- common/config/nvim/lua/options.lua
+++ common/config/nvim/lua/options.lua
@@ -1,13 +1,17 @@
 local opt = vim.opt
 
 -- core
+opt.encoding = "utf-8"
 opt.fileencoding = "utf-8"
+vim.scriptencoding = "utf-8"
 
 -- appearance
 opt.number = true
 opt.cursorline = true
 opt.virtualedit = "onemore"
 opt.smartindent = true
+opt.list = true
+opt.listchars = { tab = ">>", trail = "-", nbsp = "+" }
 
 -- tabs
 opt.expandtab = vim.fn.expand("%:r") ~= 'Makefile' -- Makefileのときはタブ、それ以外はスペース
@@ -20,3 +24,7 @@ opt.smartcase = true
 opt.incsearch = true
 opt.hlsearch = true
 opt.wrapscan = true
+
+-- backup
+opt.backup = false
+opt.swapfile = false
