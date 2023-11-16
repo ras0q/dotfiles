94f4f4578b5e38d7dc49167df8496d68c1000ecc
ras0q <ras0q@users.noreply.github.com>
2023-11-17 02:30:28 +0900

    :recycle: rename wsl2 to ubuntu

--- ubuntu/tasks/1_symbolic_links.sh
+++ ubuntu/tasks/1_symbolic_links.sh
@@ -12,7 +12,7 @@ sudo ln -sf $confDir/nvim $HOME/.config
 sudo ln -sf $confDir/starship.toml $HOME/.config
 
 # OS specific files
-confDir="$PWD/wsl2/config"
+confDir="$PWD/ubuntu/config"
 ## $HOME
 sudo ln -sf $confDir/.Brewfile $HOME
 sudo ln -sf $confDir/.gitconfig $HOME
