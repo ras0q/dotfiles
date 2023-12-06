79118f0c8259d11197b0f3dbbaea17d4cbffe402
ras0q <ras0q@users.noreply.github.com>
2023-12-07 01:40:36 +0900

    :arrow_up: update docker script

--- ubuntu/tasks/3_setup_docker.sh
+++ ubuntu/tasks/3_setup_docker.sh
@@ -1,20 +1,20 @@
 #!/bin/sh -eux
 
-# Install Docker
-sudo apt-get install -y \
-  ca-certificates \
-  curl \
-  gnupg \
-  lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg;
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" \
-  | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
-sudo apt-get update -y;
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io;
+# ref: https://docs.docker.com/engine/install/ubuntu/
 
-# Install Docker Compose
-mkdir -p ~/.docker/cli-plugins/;
-curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose;
-chmod +x ~/.docker/cli-plugins/docker-compose;
+# Add Docker's official GPG key:
+# sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install the Docker packages
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
