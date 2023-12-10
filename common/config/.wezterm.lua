08d3f74d20322be828fe8f785f72308a9892c0ec
ras0q <ras0q@users.noreply.github.com>
2023-12-10 15:12:50 +0900

    :wrench: change wezterm fontsize

--- common/config/.wezterm.lua
+++ common/config/.wezterm.lua
@@ -8,7 +8,7 @@ local config = {
     '源ノ角ゴシック Code JP',
     'Source Code Pro'
   },
-  font_size = 12.0,
+  font_size = 10.0,
   hide_tab_bar_if_only_one_tab = true,
   initial_cols = 80,
   initial_rows = 20,
