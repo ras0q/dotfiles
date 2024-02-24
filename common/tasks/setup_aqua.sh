d8cc84833ec77f3c90ce49ae291f0dc2dcbcc306
ras0q <ras0q@users.noreply.github.com>
2024-02-24 20:46:52 +0900

    :fire: remove unneeded files

--- common/tasks/setup_aqua.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -eux
-
-BRANCH="v2.2.0" # renovate: depName=aquaproj/aqua
-curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/$BRANCH/aqua-installer | bash
-
-export PATH="${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin:$PATH"
-
-aqua --version
-aqua install --all --only-link
--- common/tasks/setup_rust.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -eux
-
-# rustup
-rm -f /tmp/rustup.sh
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup.sh
-sh /tmp/rustup.sh -y --no-modify-path
-rm -f /tmp/rustup.sh
