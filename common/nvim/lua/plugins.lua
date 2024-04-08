9234dbf09c9f72ed418a72d3aeb4abcdcb12b227
ras0q <ras0q@users.noreply.github.com>
2024-04-08 11:35:46 +0900

    :fire: remove telescope-fzf-native

--- common/nvim/lua/plugins.lua
+++ common/nvim/lua/plugins.lua
@@ -37,10 +37,6 @@ require("lazy").setup({
       "nvim-tree/nvim-web-devicons",
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope-file-browser.nvim",
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-      }
     },
     -- event = "VeryLazy",
     config = function()
@@ -66,19 +62,11 @@ require("lazy").setup({
             display_stat = false,
             hijack_netrw = true,
           },
-          fzf = {
-            fuzzy = true,                    -- false will only do exact matching
-            override_generic_sorter = true,  -- override the generic sorter
-            override_file_sorter = true,     -- override the file sorter
-            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-          }
         }
       }
 
       telescope.load_extension("file_browser")
       vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>", {})
-
-      telescope.load_extension("fzf")
 
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
