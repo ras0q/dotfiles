865a3b9433da7370c95bdd380c89752ed69cd774
ras0q <ras0q@users.noreply.github.com>
2024-05-07 00:11:38 +0900

    :sparkles: add copilot

--- common/nvim/lua/plugins.lua
+++ common/nvim/lua/plugins.lua
@@ -126,6 +126,14 @@ require("lazy").setup({
     },
     config = function()
       require("noice").setup({
+          lsp = {
+            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+            override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
         presets = {
           command_palette = true, -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
@@ -134,5 +142,9 @@ require("lazy").setup({
         },
       })
     end
+  },
+  {
+    "github/copilot.vim",
+    event = "VeryLazy",
   }
 })
