3f6d5cd70da58585c3ef243d76bfd84e339e83b9
ras0q <ras0q@users.noreply.github.com>
2024-05-13 09:00:42 +0900

    :sparkles: assign <leader>fk to open keymaps

--- common/nvim/lua/plugins/finder.lua
+++ common/nvim/lua/plugins/finder.lua
@@ -44,6 +44,7 @@ return {
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>fk", builtin.keymaps, {})
 
 		require("telescope").load_extension("file_browser")
 		vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>", {})
