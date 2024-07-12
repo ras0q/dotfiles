4e9a23f73b705678e1635e2efb3d82e64c6107b9
ras0q <ras0q@users.noreply.github.com>
2024-07-12 13:39:36 +0900

    :sparkles: add deno to bin

--- common/fish/config.fish
+++ common/fish/config.fish
@@ -1,11 +1,12 @@
 # $fish_user_paths
 fish_add_path \
-  ~/.local/share/aquaproj-aqua/bin \
-  ~/go/bin \
-  ~/.rye/shims \
-  ~/.volta/bin \
-  ~/.cargo/bin \
-  /opt/homebrew/bin
+    ~/.local/share/aquaproj-aqua/bin \
+    ~/go/bin \
+    ~/.rye/shims \
+    ~/.volta/bin \
+    ~/.cargo/bin \
+    ~/.deno/bin \
+    /opt/homebrew/bin
 set -q CODE_BIN; and fish_add_path $CODE_BIN
 set -q PWSH_BIN; and fish_add_path $PWSH_BIN
 
@@ -14,8 +15,8 @@ fzf --fish | source
 
 # starship
 starship init fish --print-full-init \
-  | sed "s@(which starship)@(aqua which starship)@g" \
-  | source
+    | sed "s@(which starship)@(aqua which starship)@g" \
+    | source
 
 # zoxide
 zoxide init --cmd cd --hook pwd fish | source
