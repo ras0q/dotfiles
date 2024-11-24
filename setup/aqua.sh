50948008c1426151c55c14274cc289a83bd06a3d
ras0q <ras0q@users.noreply.github.com>
2024-11-24 19:23:40 +0900

    change aqua installer path

--- setup/aqua.sh
+++ setup/aqua.sh
@@ -3,11 +3,11 @@
 # REF: https://aquaproj.github.io/docs/products/aqua-installer#shell-script
 $__STEP__ "Install aqua"
 
-curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.1/aqua-installer
-echo "fb4b3b7d026e5aba1fc478c268e8fbd653e01404c8a8c6284fdba88ae62eda6a  aqua-installer" | sha256sum -c
-chmod +x aqua-installer
-./aqua-installer
-rm ./aqua-installer
+installer_path=/tmp/aqua-installer
+curl -sSfL -o $installer_path https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.0.1/aqua-installer
+echo "fb4b3b7d026e5aba1fc478c268e8fbd653e01404c8a8c6284fdba88ae62eda6a  $installer_path" | sha256sum -c
+bash $installer_path
+rm $installer_path
 
 $__STEP__ "Install aqua packages"
 
