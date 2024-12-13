4501b09c0b4bf8ac4e937fa04873ef4ac94054d3
ras0q <ras0q@users.noreply.github.com>
2024-12-13 19:04:12 +0900

    :bug: set variables first

--- common/fish/conf.d/0_variables.fish
+++ common/fish/conf.d/0_variables.fish
@@ -5,3 +5,13 @@ set -q AQUA_GLOBAL_CONFIG; or set -Ux AQUA_GLOBAL_CONFIG ~/.config/aquaproj-aqua
 set -q EDITOR; or set -Ux EDITOR hx
 set -q GOPATH; or set -Ux GOPATH ~/go
 set -q LANG; or set -Ux LANG POSIX
+
+fish_add_path \
+    ~/.local/bin \
+    ~/go/bin \
+    ~/.cargo/bin \
+    ~/.deno/bin \
+    /opt/homebrew/bin \
+    /opt/homebrew/opt/openjdk/bin
+set -q CODE_BIN; and fish_add_path $CODE_BIN
+set -q PWSH_BIN; and fish_add_path $PWSH_BIN
