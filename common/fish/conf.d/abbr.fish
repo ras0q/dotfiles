d525457f407829d5c63def794b5a478ca9e8fa79
ras0q <ras0q@users.noreply.github.com>
2025-01-19 14:25:43 +0900

    add explorer

--- common/fish/conf.d/abbr.fish
+++ common/fish/conf.d/abbr.fish
@@ -18,6 +18,7 @@ abbr -a ls eza
 # others
 abbr -a bd 'brew bundle dump --global --tap --formula -f'
 abbr -a c 'code .'
+abbr -a e '/mnt/c/Windows/explorer.exe'
 abbr -a grm 'rm -i $GOPATH/bin/(ls $GOPATH/bin | fzf)'
 abbr -a gg 'ghq get'
 abbr -a h 'hx .'
