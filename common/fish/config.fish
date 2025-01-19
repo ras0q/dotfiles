358b553c1f449f114a1ffbd477080ac211cc31c5
ras0q <ras0q@users.noreply.github.com>
2025-01-19 14:25:36 +0900

    add hint

--- common/fish/config.fish
+++ common/fish/config.fish
@@ -14,10 +14,16 @@ zoxide init --cmd cd --hook pwd fish | source
 fnm completions --shell fish | source
 
 # Zellij
-if not set -q ZELLIJ; and set -q ZELLIJ_AUTO_ATTACH
-    zellij attach -c
+if not set -q ZELLIJ
+    if set -q ZELLIJ_AUTO_ATTACH
+        zellij attach -c
 
-    if set -q ZELLIJ_AUTO_EXIT
-        kill $fish_pid
+        if set -q ZELLIJ_AUTO_EXIT
+            kill $fish_pid
+        end
+    else
+        set_color green
+        echo "You can start Zellij automatically by setting \$ZELLIJ_AUTO_ATTACH."
+        set_color normal
     end
 end
