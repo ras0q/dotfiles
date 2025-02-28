2a79696432232d5f11e649467d26563f7477514b
ras0q <ras0q@users.noreply.github.com>
2025-02-28 11:51:16 +0900

    use deno installed in script

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
-#!/bin/bash -eu
+#!/bin/bash -eux
 
-DENO_INSTALL=/tmp/.deno
+export DENO_INSTALL=/tmp/.deno
 
 curl -fsSL https://deno.land/install.sh | sh -s -- -y --no-modify-path
 
-PATH=$DENO_INSTALL/bin:$PATH
-./install.ts $@
+$DENO_INSTALL/bin/deno run -A ./install.ts $@
 rm -r $DENO_INSTALL
