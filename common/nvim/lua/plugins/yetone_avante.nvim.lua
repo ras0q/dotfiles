42629a977a914fb98447ad2c7db2edf1bd41ac74
ras0q <ras0q@users.noreply.github.com>
2025-06-03 01:30:30 +0900

    lazy load keys

--- common/nvim/lua/plugins/yetone_avante.nvim.lua
+++ common/nvim/lua/plugins/yetone_avante.nvim.lua
@@ -52,13 +52,12 @@ return {
           },
         },
       },
-      keys = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
-        vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>h', builtin.help_tags, { desc = 'Telescope help tags' })
-      end,
+      keys = {
+        { "<leader>f", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
+        { "<leader>g", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" },
+        { "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Telescope buffers" },
+        { "<leader>h", "<cmd>Telescope help_tags<cr>", desc = "Telescope help tags" },
+      },
     },
     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     "ibhagwan/fzf-lua", -- for file_selector provider fzf
