e01966fd292e30c4ca18257919db53f98eaf9c97
ras0q <ras0q@users.noreply.github.com>
2025-06-05 03:04:20 +0900

    use mini.nvim stack

---
+++ common/nvim/lua/plugins/echasnovski_mini.nvim.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "echasnovski/mini.nvim",
+    version = "*",
+    lazy = false,
+    config = function()
+      -- General workflow
+      require("mini.files").setup()
+    end,
+  },
+  {
+    "echasnovski/mini.nvim",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      -- Text editing
+      require("mini.comment").setup()
+      require("mini.completion").setup()
+      require("mini.pairs").setup()
+      require("mini.surround").setup()
+
+      -- General workflow
+      require("mini.basics").setup()
+      require("mini.diff").setup()
+      require("mini.files").setup()
+      require("mini.git").setup()
+
+    -- Appearance
+      require("mini.animate").setup()
+      require("mini.cursorword").setup()
+      require("mini.hipatterns").setup()
+      require("mini.icons").setup()
+      require("mini.indentscope").setup()
+      require("mini.notify").setup()
+      require("mini.statusline").setup()
+      require("mini.tabline").setup()
+      require("mini.trailspace").setup()
+
+      -- Other
+      require("mini.fuzzy").setup()
+    end,
+  },
+}
--- common/nvim/lua/plugins/stevearc_oil.nvim.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "stevearc/oil.nvim",
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
-}
