16a97a7c610721b84ce80e34e2cab60f8f517065
ras0q <ras0q@users.noreply.github.com>
2025-06-05 01:41:16 +0900

    add oil as a file explorer

---
+++ common/nvim/lua/plugins/stevearc_oil.nvim.lua
@@ -0,0 +1,8 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+}
