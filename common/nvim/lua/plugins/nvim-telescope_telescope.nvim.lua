674fd600bbde2791d4c019f5f76476bc1fac0ab5
ras0q <ras0q@users.noreply.github.com>
2025-06-05 03:23:49 +0900

    replace mini.files with telescope-file-browser

--- common/nvim/lua/plugins/nvim-telescope_telescope.nvim.lua
+++ common/nvim/lua/plugins/nvim-telescope_telescope.nvim.lua
@@ -8,11 +8,17 @@ return {
         prompt_position = "top",
       },
     },
+    extensions = {
+      file_browser = {
+        hijack_netrw = true,
+      }
+    }
   },
   keys = {
     { "<leader>f", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
     { "<leader>g", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" },
     { "<leader>b", "<cmd>Telescope buffers<cr>", desc = "Telescope buffers" },
     { "<leader>h", "<cmd>Telescope help_tags<cr>", desc = "Telescope help tags" },
+    { "<leader>b", "<cmd>Telescope file_browser<cr>", desc = "Telescope file browser" },
   },
 }
