f2526296e4bca3ca75e8b1b2a30da24e0815fb02
ras0q <ras0q@users.noreply.github.com>
2025-07-01 15:27:52 +0900

    disable mini.basics extra_ui

--- common/nvim/lua/plugins/echasnovski_mini.nvim.lua
+++ common/nvim/lua/plugins/echasnovski_mini.nvim.lua
@@ -41,9 +41,6 @@ return {
     lazy = false,
     config = function()
       require("mini.basics").setup({
-        options = {
-          extra_ui = true,
-        },
         mappings = {
           option_toggle_prefix = "m",
         }
