de927af4bd918890f7bb55228ad1bcc0c01628d0
ras0q <ras0q@users.noreply.github.com>
2025-07-01 12:46:10 +0900

    split nonlazy modules

--- common/nvim/lua/plugins/echasnovski_mini.nvim.lua
+++ common/nvim/lua/plugins/echasnovski_mini.nvim.lua
@@ -5,29 +5,63 @@ return {
     event = "VeryLazy",
     config = function()
       -- Text editing
-      require("mini.comment").setup()
+      require("mini.comment").setup({
+        mappings = {
+          comment = "<C-c>",
+          comment_line = "<C-c>",
+          comment_visual = "<C-c>",
+          textobject = "<C-c>",
+        }
+      })
       require("mini.completion").setup()
       require("mini.pairs").setup()
       require("mini.surround").setup()
 
       -- General workflow
-      require("mini.basics").setup()
+      -- require("mini.basics").setup() -- nonlazy
       require("mini.diff").setup()
       require("mini.git").setup()
 
     -- Appearance
-      require("mini.animate").setup()
       require("mini.cursorword").setup()
       require("mini.hipatterns").setup()
       require("mini.icons").setup()
       require("mini.indentscope").setup()
       require("mini.notify").setup()
-      require("mini.statusline").setup()
-      require("mini.tabline").setup()
+      -- require("mini.statusline").setup() -- nonlazy
+      -- require("mini.tabline").setup() -- nonlazy
       require("mini.trailspace").setup()
 
       -- Other
       require("mini.fuzzy").setup()
     end,
+  },
+  {
+    "echasnovski/mini.basics",
+    lazy = false,
+    config = function()
+      require("mini.basics").setup({
+        options = {
+          extra_ui = true,
+        },
+        mappings = {
+          option_toggle_prefix = "m",
+        }
+      })
+    end
+  },
+  {
+    "echasnovski/mini.statusline",
+    lazy = false,
+    config = function()
+      require("mini.statusline").setup()
+    end
+  },
+  {
+    "echasnovski/mini.tabline",
+    lazy = false,
+    config = function()
+      require("mini.tabline").setup()
+    end
   },
 }
