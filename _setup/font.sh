0b68f628c2c105a8c7099c42c6c5c798a0031bd0
ras0q <ras0q@users.noreply.github.com>
2025-07-01 15:27:21 +0900

    change downloaded font urls

--- _setup/font.sh
+++ _setup/font.sh
@@ -7,10 +7,8 @@ cd ./dist
 
 # 源ノ角ゴシック
 curl -O "https://github.com/adobe-fonts/source-han-sans/raw/release/Variable/OTF/Subset/SourceHanSansJP-VF.otf"
-# 源ノ角ゴシック Code
-curl -O "https://github.com/adobe-fonts/source-han-code-jp/raw/release/OTF/SourceHanCodeJP-Regular.otf"
-# Source Code Pro (Nerd Font)
-curl -O "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/SauceCodeProNerdFont-Regular.ttf"
+# 源ノ角ゴシック Code JP (Use OTF!)
+curl -O "https://github.com/adobe-fonts/source-han-code-jp/archive/refs/heads/release.zip"
 # Nerd Font
 curl -O "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/NerdFontsSymbolsOnly/SymbolsNerdFont-Regular.ttf"
 
