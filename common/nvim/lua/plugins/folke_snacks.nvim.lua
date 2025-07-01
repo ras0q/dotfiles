df28485517354e2317eba0772d9fff4d3387418c
ras0q <ras0q@users.noreply.github.com>
2025-07-01 14:58:02 +0900

    replace telescope with snacks

--- common/nvim/lua/plugins/folke_snacks.nvim.lua
+++ common/nvim/lua/plugins/folke_snacks.nvim.lua
@@ -1,10 +1,19 @@
-local enabled = { enabled = true }
-
--- for avante.nvim's input provider
+-- for avante.nvim's input provider and replacing telescope functionality
 return {
   "folke/snacks.nvim",
   lazy = false,
+  ---@type snacks.Config
   opts = {
-    picker = enabled,
-  }
+    explorer = { enabled = true },
+    picker = { enabled = true },
+  },
+  keys = {
+    { "<leader>b", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader>c", function() Snacks.picker.commands() end, desc = "Commands" },
+    { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+    { "<leader>f", function() Snacks.picker.files() end, desc = "Find Files" },
+    { "<leader>g", function() Snacks.picker.grep() end, desc = "Live Grep" },
+    { "<leader>h", function() Snacks.picker.help() end, desc = "Help Tags" },
+    { "<leader>p", function() Snacks.picker.pickers() end, desc = "List Pickers" },
+  },
 }
