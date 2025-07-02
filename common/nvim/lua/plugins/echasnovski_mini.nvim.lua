5de4c2d79ab54d21ee0b0a372677b558e5fb53d4
ras0q <ras0q@users.noreply.github.com>
2025-07-03 04:36:02 +0900

    merge mini.nvim config

--- common/nvim/lua/plugins/echasnovski_mini.nvim.lua
+++ common/nvim/lua/plugins/echasnovski_mini.nvim.lua
@@ -2,68 +2,56 @@ return {
   {
     "echasnovski/mini.nvim",
     version = "*",
-    event = "VeryLazy",
-    config = function()
-      -- Text editing
-      require("mini.comment").setup({
-        mappings = {
-          comment = "<C-c>",
-          comment_line = "<C-c>",
-          comment_visual = "<C-c>",
-          textobject = "<C-c>",
-        }
-      })
-      require("mini.pairs").setup()
-      require("mini.surround").setup()
-
-      -- General workflow
-      -- require("mini.basics").setup() -- nonlazy
-      require("mini.diff").setup({
-        mappings = {
-          apply = "dh",
-          reset = "dH",
-          textobject = "dh",
-        }
-      })
-      require("mini.git").setup()
-
-      -- Appearance
-      require("mini.cursorword").setup()
-      require("mini.hipatterns").setup()
-      require("mini.icons").setup()
-      require("mini.indentscope").setup()
-      require("mini.notify").setup()
-      -- require("mini.statusline").setup() -- nonlazy
-      -- require("mini.tabline").setup() -- nonlazy
-      require("mini.trailspace").setup()
-
-      -- Other
-      require("mini.fuzzy").setup()
-    end,
-  },
-  {
-    "echasnovski/mini.basics",
     lazy = false,
     config = function()
+      -- Set up visually significant plugins during startup
       require("mini.basics").setup({
         mappings = {
           option_toggle_prefix = "m",
         }
       })
-    end
-  },
-  {
-    "echasnovski/mini.statusline",
-    lazy = false,
-    config = function()
+      require("mini.starter").setup()
       require("mini.statusline").setup()
-    end
-  },
-  {
-    "echasnovski/mini.tabline",
-    lazy = false,
-    config = function()
       require("mini.tabline").setup()
-    end
+
+      -- Lazy loading other plugins
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          -- Text editing
+          require("mini.comment").setup({
+            mappings = {
+              comment = "<C-c>",
+              comment_line = "<C-c>",
+              comment_visual = "<C-c>",
+              textobject = "<C-c>",
+            }
+          })
+          require("mini.pairs").setup()
+          require("mini.surround").setup()
+
+          -- General workflow
+          require("mini.diff").setup({
+            mappings = {
+              apply = "dh",
+              reset = "dH",
+              textobject = "dh",
+            }
+          })
+          require("mini.git").setup()
+          require("mini.sessions").setup()
+
+          -- Appearance
+          require("mini.cursorword").setup()
+          require("mini.hipatterns").setup()
+          require("mini.icons").setup()
+          require("mini.indentscope").setup()
+          require("mini.notify").setup()
+          require("mini.trailspace").setup()
+
+          -- Other
+          require("mini.fuzzy").setup()
+        end
+      })
+    end,
   },
 }
