b1e953fa9ed413c9a9489378bd41cf7644f7de55
ras0q <ras0q@users.noreply.github.com>
2025-07-03 04:36:53 +0900

    unneeded bracket

--- common/nvim/lua/plugins/echasnovski_mini.nvim.lua
+++ common/nvim/lua/plugins/echasnovski_mini.nvim.lua
@@ -1,57 +1,55 @@
 return {
-  {
-    "echasnovski/mini.nvim",
-    version = "*",
-    lazy = false,
-    config = function()
-      -- Set up visually significant plugins during startup
-      require("mini.basics").setup({
-        mappings = {
-          option_toggle_prefix = "m",
-        }
-      })
-      require("mini.starter").setup()
-      require("mini.statusline").setup()
-      require("mini.tabline").setup()
+  "echasnovski/mini.nvim",
+  version = "*",
+  lazy = false,
+  config = function()
+    -- Set up visually significant plugins during startup
+    require("mini.basics").setup({
+      mappings = {
+        option_toggle_prefix = "m",
+      }
+    })
+    require("mini.starter").setup()
+    require("mini.statusline").setup()
+    require("mini.tabline").setup()
 
-      -- Lazy loading other plugins
-      vim.api.nvim_create_autocmd("VimEnter", {
-        callback = function()
-          -- Text editing
-          require("mini.comment").setup({
-            mappings = {
-              comment = "<C-c>",
-              comment_line = "<C-c>",
-              comment_visual = "<C-c>",
-              textobject = "<C-c>",
-            }
-          })
-          require("mini.pairs").setup()
-          require("mini.surround").setup()
+    -- Lazy loading other plugins
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        -- Text editing
+        require("mini.comment").setup({
+          mappings = {
+            comment = "<C-c>",
+            comment_line = "<C-c>",
+            comment_visual = "<C-c>",
+            textobject = "<C-c>",
+          }
+        })
+        require("mini.pairs").setup()
+        require("mini.surround").setup()
 
-          -- General workflow
-          require("mini.diff").setup({
-            mappings = {
-              apply = "dh",
-              reset = "dH",
-              textobject = "dh",
-            }
-          })
-          require("mini.git").setup()
-          require("mini.sessions").setup()
+        -- General workflow
+        require("mini.diff").setup({
+          mappings = {
+            apply = "dh",
+            reset = "dH",
+            textobject = "dh",
+          }
+        })
+        require("mini.git").setup()
+        require("mini.sessions").setup()
 
-          -- Appearance
-          require("mini.cursorword").setup()
-          require("mini.hipatterns").setup()
-          require("mini.icons").setup()
-          require("mini.indentscope").setup()
-          require("mini.notify").setup()
-          require("mini.trailspace").setup()
+        -- Appearance
+        require("mini.cursorword").setup()
+        require("mini.hipatterns").setup()
+        require("mini.icons").setup()
+        require("mini.indentscope").setup()
+        require("mini.notify").setup()
+        require("mini.trailspace").setup()
 
-          -- Other
-          require("mini.fuzzy").setup()
-        end
-      })
-    end,
-  },
+        -- Other
+        require("mini.fuzzy").setup()
+      end
+    })
+  end,
 }
