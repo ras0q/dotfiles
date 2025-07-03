ea09953242f9a7308fc3547635c04e51b848c8b2
ras0q <ras0q@users.noreply.github.com>
2025-07-03 19:41:22 +0900

    fix template

--- common/nvim/lua/config/commands.lua
+++ common/nvim/lua/config/commands.lua
@@ -9,7 +9,7 @@ vim.api.nvim_create_user_command("AddPlugin", function(opts)
 
   local file = io.open(path, "w")
   if file then
-    file:write(string.format('return {\n  "%s",\n}\n', spec))
+    file:write(string.format('return {\n  "%s",\n  event = "VeryLazy",\n}\n', spec))
     file:close()
     vim.notify("✅ Created: " .. vim.fn.fnamemodify(path, ":~"), vim.log.levels.INFO)
   end
