6ccb8fa7a7d197aed60396dd7051aa23174f3bbf
ras0q <ras0q@users.noreply.github.com>
2025-07-03 18:36:55 +0900

    add :AddPlugin command

--- common/nvim/init.lua
+++ common/nvim/init.lua
@@ -1,3 +1,4 @@
+require("config.commands")
 require("config.keymaps")
 require("config.options")
 require("config.lazy")
---
+++ common/nvim/lua/config/commands.lua
@@ -0,0 +1,16 @@
+vim.api.nvim_create_user_command("AddPlugin", function(opts)
+  local spec = opts.fargs[1]
+  if not spec or not spec:match("^[^/]+/.+$") then
+    return vim.notify("Argument must be in 'author/repo' format", vim.log.levels.ERROR)
+  end
+
+  local path = string.format("%s/lua/plugins/%s.lua", vim.fn.stdpath("config"), spec)
+  vim.fn.mkdir(vim.fn.fnamemodify(path, ":h"), "p")
+
+  local file = io.open(path, "w")
+  if file then
+    file:write(string.format('return {\n  "%s",\n}', spec))
+    file:close()
+    vim.notify("✅ Created: " .. vim.fn.fnamemodify(path, ":~"), vim.log.levels.INFO)
+  end
+end, { nargs = 1 })
