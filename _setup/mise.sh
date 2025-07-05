6691ad17f52ed2ab9bd15dbb96e55250f1c0ad04
ras0q <ras0q@users.noreply.github.com>
2025-07-05 13:28:35 +0900

    install fish and exa in linux

--- _setup/mise.sh
+++ _setup/mise.sh
@@ -4,3 +4,7 @@ $__STEP__ "Install mise"
 
 curl https://mise.run | sh
 ~/.local/bin/mise install
+if [[ "$(uname)" == "Linux" || "$(uname)" == "Darwin" ]]; then
+  ~/.local/bin/mise install aqua:eza-community/eza
+  ~/.local/bin/mise install aqua:fish-shell/fish-shell
+fi
