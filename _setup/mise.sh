0eb4f717d9c6f5ef958ddbdd950499c0eff764b0
ras0q <ras0q@users.noreply.github.com>
2025-07-17 02:31:13 +0900

    fix command

--- _setup/mise.sh
+++ _setup/mise.sh
@@ -10,5 +10,5 @@ MISE_PATH=$(command -v mise || echo ~/.local/bin/mise)
 
 $MISE_PATH install
 if [[ "$(uname)" == "Linux" || "$(uname)" == "Darwin" ]]; then
-  $MISE_PATH install --path ~/.config/mise/config.linux.toml
+  $MISE_PATH install --env linux
 fi
