5432147d15f788663659680b722a2da13794ad5b
ras0q <ras0q@users.noreply.github.com>
2025-07-23 10:25:08 +0900

    use vim.keymap.set

--- common/nvim/lua/config/keymaps.lua
+++ common/nvim/lua/config/keymaps.lua
@@ -1,26 +1,25 @@
-local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- normal mode
-keymap("n", "j", "gj", opts)
-keymap("n", "k", "gk", opts)
-keymap("n", "<Down>", "gj", opts)
-keymap("n", "<Up>", "gk", opts)
-keymap("n", "gh", "^", opts)
-keymap("n", "gl", "$", opts)
-keymap("n", "ge", "G", opts)
-keymap("n", "gp", ":bprevious<CR>", opts)
-keymap("n", "gn", ":bnext<CR>", opts)
-keymap("n", "<Space><Space>", "ZZ", opts)
-keymap("n", "<Plug>(esc)<ESC>", "i<ESC>", opts) -- back to terminal mode
+vim.keymap.set("n", "j", "gj", opts)
+vim.keymap.set("n", "k", "gk", opts)
+vim.keymap.set("n", "<Down>", "gj", opts)
+vim.keymap.set("n", "<Up>", "gk", opts)
+vim.keymap.set("n", "gh", "^", opts)
+vim.keymap.set("n", "gl", "$", opts)
+vim.keymap.set("n", "ge", "G", opts)
+vim.keymap.set("n", "gp", ":bprevious<CR>", opts)
+vim.keymap.set("n", "gn", ":bnext<CR>", opts)
+vim.keymap.set("n", "<Space><Space>", "ZZ", opts)
+vim.keymap.set("n", "<Plug>(esc)<ESC>", "i<ESC>", opts) -- back to terminal mode
 
 -- insert mode
-keymap("i", "jk", "<ESC>", opts)
+vim.keymap.set("i", "jk", "<ESC>", opts)
 
 -- visual mode
-keymap("v", "gh", "g^", opts)
-keymap("v", "gl", "g$", opts)
-keymap("v", "ge", "G", opts)
+vim.keymap.set("v", "gh", "g^", opts)
+vim.keymap.set("v", "gl", "g$", opts)
+vim.keymap.set("v", "ge", "G", opts)
 
 -- terminal mode
-keymap("t", "<ESC>", "<C-\\><C-n><Plug>(esc)", opts)
+vim.keymap.set("t", "<ESC>", "<C-\\><C-n><Plug>(esc)", opts)
