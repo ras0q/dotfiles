603687ea1d5e3ac577cbe1900b6a16a0d8421fe5
ras0q <ras0q@users.noreply.github.com>
2025-08-12 03:12:56 +0900

    use transparent background

--- common/nvim/lua/plugins/catppuccin/nvim.lua
+++ common/nvim/lua/plugins/catppuccin/nvim.lua
@@ -5,6 +5,11 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    require("catppuccin").setup({
+      flavour = "auto",
+      transparent_background = true,
+      auto_integrations = true,
+    })
     vim.cmd.colorscheme "catppuccin-latte"
   end,
 }
