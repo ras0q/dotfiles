ed103b372c007e5817310e8190ac9a71f93cf877
ras0q <ras0q@users.noreply.github.com>
2025-08-20 11:35:48 +0900

    chore(lazy.nvim): disable checker

--- common/nvim/lua/config/lazy.lua
+++ common/nvim/lua/config/lazy.lua
@@ -50,7 +50,7 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  -- checker = { enabled = true },
   ui = {
     border = "rounded",
   },
