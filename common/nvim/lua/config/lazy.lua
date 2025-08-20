1063cb969e445c227fc7df5bb109e88f2256e73d
ras0q <ras0q@users.noreply.github.com>
2025-08-20 10:05:10 +0900

    chore(lazy.nvim): add border

--- common/nvim/lua/config/lazy.lua
+++ common/nvim/lua/config/lazy.lua
@@ -36,7 +36,7 @@ for _, dir in ipairs(subdirs) do
           :gsub(sep, ".")
           :gsub("^%.", "")
       table.insert(specs, { import = module })
-   end
+    end
   end
 end
 
@@ -50,5 +50,8 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  -- checker = { enabled = true },
+  checker = { enabled = true },
+  ui = {
+    border = "rounded",
+  },
 })
