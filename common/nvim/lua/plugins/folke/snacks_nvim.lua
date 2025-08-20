ef5065bd51d7552ab8910b22a0e35684364c363d
ras0q <ras0q@users.noreply.github.com>
2025-08-21 07:23:02 +0900

    feat(snacks): add keymap to recent files

--- common/nvim/lua/plugins/folke/snacks_nvim.lua
+++ common/nvim/lua/plugins/folke/snacks_nvim.lua
@@ -1,6 +1,21 @@
+local list_repos = {
+  finder = "proc",
+  cmd = "ghq",
+  args = { "list", "--full-path" },
+  transform = function(item)
+    item.file = item.text
+    item.dir = true
+  end,
+  confirm = function(picker, item)
+    picker:close()
+    vim.cmd("cd " .. item.text)
+  end,
+}
+
 -- for avante.nvim's input provider and replacing telescope functionality
 return {
   "folke/snacks.nvim",
+  version = "v2.22.0",
   lazy = false,
   priority = 1000,
   ---@type snacks.Config
@@ -25,27 +40,10 @@ return {
     { "<leader>b", function() Snacks.picker.buffers({ hidden = true }) end, desc = "Buffers" },
     { "<leader>e", function() Snacks.explorer() end,                        desc = "File Explorer" },
     { "<leader>f", function() Snacks.picker.files({ hidden = true }) end,   desc = "Find Files" },
+    { "<leader>h", function() Snacks.picker(list_repos) end,                desc = "List Repositories" },
     { "<leader>g", function() Snacks.picker.grep({ hidden = true }) end,    desc = "Live Grep" },
     { "<leader>l", function() Snacks.lazygit() end,                         desc = "LazyGit" },
     { "<leader>p", function() Snacks.picker.pickers() end,                  desc = "List Pickers" },
-    {
-      "<leader>h",
-      function()
-        Snacks.picker({
-          finder = "proc",
-          cmd = "ghq",
-          args = { "list", "--full-path" },
-          transform = function(item)
-            item.file = item.text
-            item.dir = true
-          end,
-          confirm = function(picker, item)
-            picker:close()
-            vim.cmd("cd " .. item.text)
-          end,
-        })
-      end,
-      desc = "List Repositories",
-    },
+    { "<leader>r", function() Snacks.picker.recent() end,                   desc = "List Recent files" },
   },
 }
