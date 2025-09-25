f4d73c7a578d3ad446cd5758f7bfce193be38e1d
ras0q <ras0q@users.noreply.github.com>
2025-09-25 17:39:34 +0900

    fix(nvim): close completion popup on inline completion accepted

--- common/nvim/lua/config/lsp.lua
+++ common/nvim/lua/config/lsp.lua
@@ -66,15 +66,19 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end
 
     if client:supports_method("textDocument/inlineCompletion") then
-      vim.lsp.inline_completion.enable()
+      vim.lsp.inline_completion.enable(true, { bufnr = buf })
       vim.keymap.set("i", "<Tab>", function()
         if not vim.lsp.inline_completion.get() then
           return "<Tab>"
         end
+        -- close the completion popup if it's open
+        if vim.fn.pumvisible() == 1 then
+          return "<C-y>"
+        end
       end, {
         expr = true,
-        replace_keycodes = true,
-        desc = "Get the current inline completion",
+        buffer = buf,
+        desc = "Accept the current inline completion",
       })
     end
   end,
