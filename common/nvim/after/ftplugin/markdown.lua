7b5425d7122185cdcf7402476a511544593d74a0
ras0q <ras0q@users.noreply.github.com>
2025-10-17 23:34:45 +0900

    chore: fix markdown ft

--- common/nvim/after/ftplugin/markdown.lua
+++ common/nvim/after/ftplugin/markdown.lua
@@ -1,14 +1,29 @@
+vim.opt.spell = true
+vim.opt.spelllang = { "en", "cjk" }
+local function get_eol()
+  local ff = vim.bo.fileformat
+  if ff == "dos" then
+    return "\r\n"
+  elseif ff == "mac" then
+    return "\r"
+  else
+    return "\n"
+  end
+end
+
+
 vim.keymap.set("i", "<CR>", function()
   local line = vim.api.nvim_get_current_line()
   local indent, bullet, rest = line:match("^(%s*)([%-%+%*]%s+[ xX]?)%s*(.*)$")
-  local cr = vim.api.nvim_replace_termcodes("<CR>", true, false, true)
 
   if not bullet then
-    return cr
+    return vim.api.nvim_replace_termcodes("<CR>", true, false, true)
   end
 
+  local eol = get_eol()
+
   if #rest > 0 then
-    return cr .. bullet
+    return eol .. bullet
   end
 
   if #indent > 0 then
