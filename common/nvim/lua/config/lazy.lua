d1a55763e62d8cdb008b38f129829083e45418b9
ras0q <ras0q@users.noreply.github.com>
2025-10-18 17:01:37 +0900

    fix(nvim): fix colorscheme

--- common/nvim/lua/config/lazy.lua
+++ common/nvim/lua/config/lazy.lua
@@ -56,6 +56,6 @@ require("lazy").setup({
   },
 })
 
-vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("catppuccin-latte")
 
 vim.keymap.set("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
