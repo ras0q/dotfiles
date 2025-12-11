994cbaa09352f9716b5237e162e65b88f877c11a
ras0q <ras0q@users.noreply.github.com>
2025-12-11 22:19:58 +0900

    fix(nvim): fix MCP on CodeCompanion

--- common/nvim/lua/plugins/olimorris/codecompanion_nvim.lua
+++ common/nvim/lua/plugins/olimorris/codecompanion_nvim.lua
@@ -1,18 +1,21 @@
+local default_adapter = {
+  name = "copilot",
+  model = "gpt-4.1",
+}
+
 ---@type LazyPluginSpec
 return {
   "olimorris/codecompanion.nvim",
   enabled = require("config.options").ai_enabled,
   dependencies = {
     "ravitemer/codecompanion-history.nvim",
+    { "ravitemer/mcphub.nvim", opts = {} },
     "franco-ruggeri/codecompanion-spinner.nvim",
   },
   opts = {
     strategies = {
       chat = {
-        adapter = {
-          name = "copilot",
-          model = "gpt-5-mini",
-        },
+        adapter = default_adapter,
         roles = {
           llm = function(adapter)
             local model = (adapter and adapter.model) and adapter.model.name or "unknown"
@@ -23,21 +26,16 @@ return {
           opts = {
             default_tools = {
               "full_stack_dev",
+              "mcp",
             },
           },
         },
       },
       inline = {
-        adapter = {
-          name = "copilot",
-          model = "gpt-4.1",
-        },
+        adapter = default_adapter,
       },
       agent = {
-        adapter = {
-          name = "copilot",
-          model = "gpt-4.1",
-        },
+        adapter = default_adapter,
       },
     },
     display = {
@@ -55,24 +53,18 @@ return {
     },
     extensions = {
       history = {
-        title_generation_opts = {
-          adapter = "copilot",
-          model = "gpt-4.1",
-        },
+        title_generation_opts = default_adapter,
       },
       mcphub = {
         callback = "mcphub.extensions.codecompanion",
         opts = {
           -- MCP Tools
-          make_tools = true,                    -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
-          show_server_tools_in_chat = true,     -- Show individual tools in chat completion (when make_tools=true)
-          add_mcp_prefix_to_tool_names = false, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
-          show_result_in_chat = true,           -- Show tool results directly in chat buffer
-          format_tool = nil,                    -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
+          show_result_in_chat = true, -- Show tool results directly in chat buffer
+          format_tool = nil,          -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
           -- MCP Resources
-          make_vars = true,                     -- Convert MCP resources to #variables for prompts
+          make_vars = true,           -- Convert MCP resources to #variables for prompts
           -- MCP Prompts
-          make_slash_commands = true,           -- Add MCP prompts as /slash commands
+          make_slash_commands = true, -- Add MCP prompts as /slash commands
         },
       },
       spinner = {},
