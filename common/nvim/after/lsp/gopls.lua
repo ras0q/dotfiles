76198c5661db9031b657284521b11bc0ce652b62
ras0q <ras0q@users.noreply.github.com>
2025-12-12 10:53:43 +0900

    docs(nvim): add ref to gopls mcp

--- common/nvim/after/lsp/gopls.lua
+++ common/nvim/after/lsp/gopls.lua
@@ -1,4 +1,7 @@
 ---@type vim.lsp.Config
 return {
+  -- https://go.dev/gopls/features/mcp#attached-mode
+  -- codex mcp add gopls --url http://localhost:8092
+  -- gopls mcp -instructions > /path/to/contextFile.md
   cmd = { "gopls", "serve", "-mcp.listen=localhost:8092" },
 }
