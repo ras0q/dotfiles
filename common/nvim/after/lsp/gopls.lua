fac86e96bd1a1150a325486c2aa29972e8d0c45e
ras0q <ras0q@users.noreply.github.com>
2025-12-14 22:39:53 +0900

    fix(nvim): disable gopls mcp attached mode

--- common/nvim/after/lsp/gopls.lua
+++
@@ -1,7 +0,0 @@
----@type vim.lsp.Config
-return {
-  -- https://go.dev/gopls/features/mcp#attached-mode
-  -- codex mcp add gopls --url http://localhost:8092
-  -- gopls mcp -instructions > /path/to/contextFile.md
-  cmd = { "gopls", "serve", "-mcp.listen=localhost:8092" },
-}
