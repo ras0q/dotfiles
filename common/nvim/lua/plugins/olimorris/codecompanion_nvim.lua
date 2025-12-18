a585461ca47a727596e123bdedac7a6855f93408
ras0q <ras0q@users.noreply.github.com>
2025-12-19 00:59:09 +0900

    chore(nvim): disable mcp (all servers) on default

--- common/nvim/lua/plugins/olimorris/codecompanion_nvim.lua
+++ common/nvim/lua/plugins/olimorris/codecompanion_nvim.lua
@@ -33,8 +33,10 @@ return {
             auto_submit_errors = true,
             auto_submit_success = true,
             default_tools = {
+              -- NOTE: Following tools are also useful, but disabled by default to reduce the total number of tools.
+              -- - @fetch_webpage
+              -- - @mcp__SERVERNAME
               "full_stack_dev",
-              "mcp",
             },
           },
         },
@@ -77,15 +79,15 @@ return {
         callback = "mcphub.extensions.codecompanion",
         opts = {
           -- MCP Tools
-          make_tools = true,                    -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
-          show_server_tools_in_chat = true,     -- Show individual tools in chat completion (when make_tools=true)
-          add_mcp_prefix_to_tool_names = false, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
-          show_result_in_chat = true,           -- Show tool results directly in chat buffer
-          format_tool = nil,                    -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
+          make_tools = true,                   -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
+          show_server_tools_in_chat = true,    -- Show individual tools in chat completion (when make_tools=true)
+          add_mcp_prefix_to_tool_names = true, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
+          show_result_in_chat = true,          -- Show tool results directly in chat buffer
+          format_tool = nil,                   -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
           -- MCP Resources
-          make_vars = true,                     -- Convert MCP resources to #variables for prompts
+          make_vars = true,                    -- Convert MCP resources to #variables for prompts
           -- MCP Prompts
-          make_slash_commands = true,           -- Add MCP prompts as /slash commands
+          make_slash_commands = true,          -- Add MCP prompts as /slash commands
         },
       },
     },
