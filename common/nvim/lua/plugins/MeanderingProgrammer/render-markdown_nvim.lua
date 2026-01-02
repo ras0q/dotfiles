cca107c51e986aea8bc15b46941ff988387f24fc
ras0q <ras0q@users.noreply.github.com>
2026-01-02 20:09:43 +0900

    chore(nvim): enable render-markdown

--- common/nvim/lua/plugins/MeanderingProgrammer/render-markdown_nvim.lua
+++ common/nvim/lua/plugins/MeanderingProgrammer/render-markdown_nvim.lua
@@ -2,11 +2,10 @@ local file_types = { "markdown", "Avante", "codecompanion" }
 
 ---@type LazyPluginSpec
 return {
-  -- Make sure to set this up properly if you have lazy=true
   "MeanderingProgrammer/render-markdown.nvim",
-  enabled = false,
   opts = {
     file_types = file_types,
+    heading = { enabled = false },
   },
   ft = file_types,
 }
