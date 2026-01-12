9cb9fb864e02154f8036fe0af0c24c6c42eadc81
ras0q <ras0q@users.noreply.github.com>
2026-01-12 10:21:20 +0900

    chore(nvim): check lazy.nvim updates

--- common/nvim/lua/config/lazy.lua
+++ common/nvim/lua/config/lazy.lua
@@ -40,11 +40,9 @@ require("lazy").setup({
     lazy = true,
   },
   spec = specs,
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  -- checker = { enabled = true },
+  checker = {
+    enabled = true,
+  },
   ui = {
     border = "rounded",
   },
