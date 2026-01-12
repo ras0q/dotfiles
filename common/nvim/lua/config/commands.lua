a476922f3de055e1a907007df82b80b28b352c3a
ras0q <ras0q@users.noreply.github.com>
2026-01-12 09:58:10 +0900

    chore(nvim): close special windows

--- common/nvim/lua/config/commands.lua
+++ common/nvim/lua/config/commands.lua
@@ -43,3 +43,20 @@ vim.api.nvim_create_user_command("SearchInBrowser", function()
     end
   end)
 end, {})
+
+-- https://zenn.dev/vim_jp/articles/ff6cd224fab0c7
+vim.api.nvim_create_autocmd("QuitPre", {
+  callback = function()
+    local current_win = vim.api.nvim_get_current_win()
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      if win ~= current_win then
+        local buf = vim.api.nvim_win_get_buf(win)
+        if vim.bo[buf].buftype == "" then
+          return
+        end
+      end
+    end
+    vim.cmd.only({ bang = true })
+  end,
+  desc = "Close all special buffers and quit Neovim",
+})
