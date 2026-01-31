427d908e002998984ab84628571daa36ae9837ae
ras0q <ras0q@users.noreply.github.com>
2026-01-31 12:24:47 +0900

    chore(nvim): enable ls server explicitly

--- common/nvim/lua/config/lsp.lua
+++ common/nvim/lua/config/lsp.lua
@@ -8,6 +8,20 @@ vim.opt.pumheight = 15
 
 vim.lsp.log.set_level("warn")
 
+--- @param server string
+--- @param cmd? string
+local function enable_server(server, cmd)
+  if vim.fn.executable(cmd or server) == 1 then
+    vim.lsp.enable(server)
+  else
+    vim.notify(string.format("LSP server '%s' not enabled: executable not found", server), vim.log.levels.WARN)
+  end
+end
+
+enable_server("denols", "deno")
+enable_server("gopls")
+enable_server("lua_ls", "lua-language-server")
+
 -- NOTE: some plugins handle LSP-related configurations:
 -- - Language server configuration: nvim-lspconfig
 -- - Language server installation: mason-lspconfig.nvim
