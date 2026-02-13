f99b6b1626373349ab9a5c991921e553314f70c2
ras0q <ras0q@users.noreply.github.com>
2026-02-13 21:41:33 +0900

    fix(nvim): setup mason-lspconfig

--- common/nvim/lua/plugins/mason-org/mason-lspconfig_nvim.lua
+++ common/nvim/lua/plugins/mason-org/mason-lspconfig_nvim.lua
@@ -2,6 +2,7 @@
 return {
   "mason-org/mason-lspconfig.nvim",
   event = { "BufReadPre", "BufNewFile" },
+  opts = {},
   dependencies = {
     { "mason-org/mason.nvim" },
     { "neovim/nvim-lspconfig" },
