17bcf873e5586e19e3d3d86d77c3900f7a671463
ras0q <ras0q@users.noreply.github.com>
2026-03-23 16:37:08 +0900

    chore(nvim): remove unused plugins

--- common/nvim/lua/plugins/pwntester/octo_nvim.lua
+++
@@ -1,9 +0,0 @@
----@type LazyPluginSpec
-return {
-  "pwntester/octo.nvim",
-  cmd = "Octo",
-  opts = {
-    picker = "snacks",
-    enable_builtin = true,
-  },
-}
--- common/nvim/lua/plugins/terryma/vim-expand-region.lua
+++
@@ -1,9 +0,0 @@
----@type LazyPluginSpec
-return {
-  "terryma/vim-expand-region",
-  event = { "BufReadPre", "BufNewFile" },
-  keys = {
-    { "v", "<Plug>(expand_region_expand)", mode = "v", desc = "Expand Region" },
-    { "V", "<Plug>(expand_region_shrink)", mode = "v", desc = "Shrink Region" },
-  },
-}
