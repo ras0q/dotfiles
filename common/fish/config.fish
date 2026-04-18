ba9cb52518d722bf4115424add23a36b7ef6b035
ras0q <ras0q@users.noreply.github.com>
2026-04-18 23:48:42 +0900

    fix(fish): dont activate mise
    https://zenn.dev/ras96/scraps/bf117461ded0dd

--- common/fish/config.fish
+++ common/fish/config.fish
@@ -1,8 +1,4 @@
 if status is-interactive
-  # mise
-  mise activate fish | source
-  mise completion fish | source
-
   fzf --fish | source
   git-wt --init fish | source
   zoxide init --cmd cd --hook pwd fish | source
