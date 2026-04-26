b19cbbb22409ca46801c10afba09b554f30b9127
ras0q <ras0q@users.noreply.github.com>
2026-04-26 19:48:09 +0900

    chore(fish): activate mise

--- common/fish/config.fish
+++ common/fish/config.fish
@@ -1,4 +1,8 @@
 if status is-interactive
+  # mise
+  mise activate fish | source
+  mise completion fish | source
+
   fzf --fish | source
   git-wt --init fish | source
   zoxide init --cmd cd --hook pwd fish | source
