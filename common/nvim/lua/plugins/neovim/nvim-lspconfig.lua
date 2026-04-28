b28654c30745cd83f054391c6e88a9ded0d78b29
ras0q <ras0q@users.noreply.github.com>
2026-04-29 00:52:35 +0900

    fix(nvim): enable language servers after nvim-lspconfig loaded

--- common/nvim/lua/plugins/neovim/nvim-lspconfig.lua
+++ common/nvim/lua/plugins/neovim/nvim-lspconfig.lua
@@ -1,4 +1,22 @@
+--- @param server string
+--- @param cmd? string
+local function enable_server(server, cmd)
+  if vim.fn.executable(cmd or server) == 1 then
+    vim.lsp.enable(server)
+  else
+    vim.notify(string.format("LSP server '%s' not enabled: executable not found", server), vim.log.levels.WARN)
+  end
+end
+
+--- @type LazyPluginSpec
 return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    enable_server("denols", "deno")
+    enable_server("gopls")
+    enable_server("lua_ls", "lua-language-server")
+    enable_server("sourcekit", "sourcekit-lsp")
+    enable_server("tsgo")
+  end,
 }
