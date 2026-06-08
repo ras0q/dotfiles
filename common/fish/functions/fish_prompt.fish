5cc5fc9a4f331352d012eede09fa9d4195609138
ras0q <ras0q@users.noreply.github.com>
2026-06-09 08:59:28 +0900

    chore(fish): override dirtystate emoji

--- common/fish/functions/fish_prompt.fish
+++ common/fish/functions/fish_prompt.fish
@@ -1,6 +1,8 @@
 function fish_prompt --description 'Write out the prompt'
         set -l last_pipestatus $pipestatus
         set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+
+        set -g ___fish_git_prompt_char_dirtystate '+'
     
         if not set -q __fish_git_prompt_show_informative_status
                 set -g __fish_git_prompt_show_informative_status 1
