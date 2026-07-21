1b761361623e14ae68d321929db8e23a386bbd49
ras0q <ras0q@users.noreply.github.com>
2026-07-21 12:23:10 +0900

    chore(nvim): add keymaps

--- common/nvim/lua/config/keymaps.lua
+++ common/nvim/lua/config/keymaps.lua
@@ -1,12 +1,21 @@
 local opts = { noremap = true, silent = true }
 
+local function open_directory_buffer()
+  local current = vim.api.nvim_buf_get_name(0)
+  local target = current ~= "" and vim.fs.dirname(current) or vim.uv.cwd() or "."
+  vim.cmd.edit(vim.fn.fnameescape(target))
+end
+
 -- normal mode
 vim.keymap.set("n", "gh", "^", opts)
 vim.keymap.set("n", "gl", "$", opts)
+vim.keymap.set("n", "<Leader>e", open_directory_buffer, { desc = "Open directory" })
 vim.keymap.set("n", "<Leader>o", ":SearchInBrowser<CR>", opts)
 vim.keymap.set("n", "<Plug>(esc)<ESC>", "i<ESC>", opts) -- back to terminal mode
 vim.keymap.set("n", "<Leader>p", '"+p', opts)
 vim.keymap.set("n", "<Leader>y", '"+y', opts)
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
 
 -- insert mode
 vim.keymap.set("i", "jk", "<ESC>", opts)
@@ -51,4 +60,5 @@ vim.cmd([[
   " Terminal
   cabb ht split <bar> resize 10 <bar> terminal
   cabb vt vsplit <bar> terminal
+  cabb tt tab terminal
 ]])
