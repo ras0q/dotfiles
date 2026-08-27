a74fce86565cfa13c990c7a41c05e824bcbfa93c
ras0q <ras0q@users.noreply.github.com>
2026-08-27 11:40:28 +0900

    chore(nvim): remove unused settings

--- common/nvim/after/ftplugin/markdown.lua
+++ common/nvim/after/ftplugin/markdown.lua
@@ -3,35 +3,3 @@ vim.opt_local.linebreak = false
 
 vim.opt_local.spell = true
 vim.opt_local.spelllang = { "en", "cjk" }
--- local function get_eol()
---   local ff = vim.bo.fileformat
---   if ff == "dos" then
---     return "\r\n"
---   elseif ff == "mac" then
---     return "\r"
---   else
---     return "\n"
---   end
--- end
---
---
--- vim.keymap.set("i", "<CR>", function()
---   local line = vim.api.nvim_get_current_line()
---   local indent, bullet, rest = line:match("^(%s*)([%-%+%*]%s+[ xX]?)%s*(.*)$")
---
---   if not bullet then
---     return vim.api.nvim_replace_termcodes("<CR>", true, false, true)
---   end
---
---   local eol = get_eol()
---
---   if #rest > 0 then
---     return eol .. bullet
---   end
---
---   if #indent > 0 then
---     return vim.api.nvim_replace_termcodes("<C-d>", true, false, true)
---   end
---
---   return vim.api.nvim_replace_termcodes("<Esc>cc<Esc>o", true, false, true)
--- end, { buffer = true, expr = true })
--- common/nvim/after/ftplugin/plaintex.lua
+++
@@ -1,6 +0,0 @@
-vim.opt_local.wrap = true
--- vim.opt_local.breakindent = true
-vim.opt_local.breakat = " ,.\\%![]()（）:;"
-
-vim.opt_local.spell = true
-vim.opt_local.spelllang = { "en", "cjk" }
